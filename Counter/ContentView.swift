17b4dbb3dd56724075f1a5ba28cc2f032715ef0c
Aaron Cleveland <toscleveland@gmail.com>
2023-12-22 20:01:13 -0600

    Initial Commit

---
+++ Counter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Counter
+//
+//  Created by Aaron Cleveland on 12/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Aaron Cleveland on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
